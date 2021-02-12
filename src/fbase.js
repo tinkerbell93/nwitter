282a7788d7f048666d0aaa65dc4c7c35171139e8
tinkerbell93 <hhjj105@gmail.com>
2021-02-13 00:01:39 +0900

    init router and firebase

---
+++ src/fbase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJhygLqBINTh-dSL9MV1u6a2RNmfigALQ",
+  authDomain: "nwitter-a93d6.firebaseapp.com",
+  projectId: "nwitter-a93d6",
+  storageBucket: "nwitter-a93d6.appspot.com",
+  messagingSenderId: "171368281048",
+  appId: "1:171368281048:web:952192d5f91a342f2978ad"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
