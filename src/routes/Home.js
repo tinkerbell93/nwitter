7c6c83717653b5681708d60e4cceeffa0faee167
tinkerbell93 <hhjj105@gmail.com>
2021-02-13 21:51:13 +0900

    Nweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,29 @@
-import React from "react";
+import { dbService } from 'fbase';
+import React, { useState } from "react";
 
 export default function Home() {
+  const [nweet, setNweet] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createdAt: Date.now(),
+    })
+    setNweet("");
+  };
+
+  const onChange = (event) => {
+    const { target: { value } } = event;
+    setNweet(value);
+  };
+
   return (
-    <span>Home</span>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={nweet} type="text" placeholder="what's on your mind?" maxLength={120} onChange={onChange} />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
   )
 } 
