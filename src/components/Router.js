5295df9ced244e20c7b14226a869b57d55f2c716
tinkerbell93 <hhjj105@gmail.com>
2021-02-13 19:55:26 +0900

    feat: Log out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,23 +1,31 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import Profile from 'routes/Profile';
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
+import Navigation from './Navigation';
 
 const AppRouter = ({ isLoggedIn }) => {
 
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
           </>
         ) : (
-            <Route exact path="/">
-              <Auth />
-            </Route>
+            <>
+              <Route exact path="/">
+                <Auth />
+              </Route>
+            </>
           )}
       </Switch>
     </Router>
